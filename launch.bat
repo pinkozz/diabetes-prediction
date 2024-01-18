5be737c883cca8d6cc75dfa01832acf2add14d19
Serhii Bohush <136079534+pinkozz@users.noreply.github.com>
2024-01-18 21:05:51 +0200

    Delete launch.bat

--- launch.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET var=%cd%
-
-start cmd /c "cd %var%\diabetes-python && start run_py.bat"
-start cmd /c "cd %var%\diabetes-prediction-react && npm start"
-
-echo Application is running. Close this window to stop the servers.
